ba082326f80b2d80fd034b33ee820a5a15d04efa
solange25 <mariesolange@siriusschool.be>
2020-07-06 13:23:27 +0200

    quote day

--- Exercices/exercice-quote-day/script.js
+++ Exercices/exercice-quote-day/script.js
@@ -1,1 +1,12 @@
 /* C'est ici qu'il faut rédiger ton code pour utiliser l'API */
+let randomNumber = Math.floor(Math.random() * Math.floor(20));
+fetch('https://type.fit/api/quotes')
+  .then(Response => {
+    return Response.json();
+  })
+  .then(Response => {
+    console.log(Response);
+  });
+let data = response[randomNumber];
+document.getElementById('quote').textContent = data.text;
+document.getElementById('author').textContent = data.author;
